13bacb9efa2c9326d3a5616f6dd9e68ba0a62dfb
Theodore Giannopoulos <t.giannopoulos@syntax.gr>
2017-08-25 21:48:08 +0300

    Added electron-is-dev and electron-debug. DevTools open only in dev mode

--- electron/main.js
+++ electron/main.js
@@ -1,33 +1,39 @@
 const electron = require('electron');
+const isDev = require('electron-is-dev');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
 
   const screen = electron.screen.getPrimaryDisplay().size;
 
-  mainWindow = new BrowserWindow({width: screen.width*0.6, height: screen.height*0.6});
-  
+  mainWindow = new BrowserWindow({ width: screen.width * 0.6, height: screen.height * 0.6 });
+
   mainWindow.setMenu(null);
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  mainWindow.webContents.openDevTools();
+  if (isDev) {
+    console.log('Running in development mode.');
+    mainWindow.webContents.openDevTools();
+  } else {
+    console.log('Running in production mode.');
+  }
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     mainWindow = null;
   })
 }
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
     createWindow();
   }
